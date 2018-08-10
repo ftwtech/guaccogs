018f7b5d02c3ac6d7c23c3823dc37f04fcba7741
ftwtech <ftwtech@mailbox.org>
2018-08-10 20:38:03 +0000

    stuff

--- triggers/__init__.py
+++ triggers/__init__.py
@@ -1,7 +1,7 @@
-from .shitpost import Shitpost
+from .triggers import Triggers
 from redbot.core import data_manager
 
 def setup(bot):
-    n = Shitpost(bot)
+    n = Triggers(bot)
     data_manager.load_bundled_data(n, __file__)
     bot.add_cog(n)
